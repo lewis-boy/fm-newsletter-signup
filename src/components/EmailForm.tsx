a1105f4fd8ab40b429ff887c9b13ad71062b341d
Luis Escobar <luisaeniebla@gmail.com>
2023-07-31 19:54:43 -0700

    cleaned the code

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -12,8 +12,8 @@ const EmailForm: FC<EmailFormProps> = ({ }) => {
                 <label className='font-bold text-[12px]'>Email Address</label>
                 <input type="email" placeholder="email@company.com" className='text-[18px] py-[13px] px-[24px] mt-[8px] border-2 border-my-grey rounded-lg' />
 
-                <div className="text-[16px] py-[15px] px-[65px] mt-[24px]
-                    bg-dark-grey-slate rounded-lg
+                <div className="text-[16px] py-[15px] px-[65px] mt-[24px] rounded-lg
+                    bg-dark-grey-slate
                     text-white font-bold relative
                     hover:shadow-[rgba(255,106,58,1.0)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]
                     transition-shadow duration-500
