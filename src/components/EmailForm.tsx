aef8cf9842bf4f0c2c7ce6e36f77f827b3b061a9
Luis Escobar <luisaeniebla@gmail.com>
2023-08-01 19:28:06 -0700

    Made the button inactive if input is wrong

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -8,7 +8,7 @@ interface EmailFormProps {
 const EmailForm: FC<EmailFormProps> = ({ }) => {
     return (
         <div className="mt-[40px]">
-            <form className='flex flex-col relative'>
+            <form className='flex flex-col relative group' noValidate>
                 <label className='font-bold text-[12px]'>Email Address</label>
                 <input
                     type="email"
@@ -35,6 +35,7 @@ const EmailForm: FC<EmailFormProps> = ({ }) => {
                     text-white font-bold relative
                     hover:shadow-[rgba(255,106,58,1.0)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]
                     transition-shadow duration-500
+                    group-invalid:shadow-none
                 "
                 >
                     Subscribe to monthly newsletter
@@ -43,23 +44,13 @@ const EmailForm: FC<EmailFormProps> = ({ }) => {
                         className="absolute inset-0 w-full h-full rounded-lg
                         text-white
                         opacity-0 hover:opacity-100 bg-gradient-to-b from-[#FF6A3A] to-[#FF527B]
-                        transition duration-500 
+                        transition duration-500
+                        group-invalid:pointer-events-none
                     "
                     >
                         Subscribe to monthly newsletter
                     </button>
                 </div>
-
-
-                {/* <button
-                    type="submit"
-                    className="text-[16px] py-[15px] px-[65px] mt-[24px] 
-                    transition-shadow ease-in-out duration-700
-                    bg-dark-grey-slate hover:bg-gradient-to-b hover:from-[#FF6A3A] hover:to-[#FF527B] hover:shadow-[rgba(255,106,58,1.0)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]
-                    text-white rounded-lg cursor-pointer w-full font-bold"
-                >
-                    Subscribe to monthly newsletter
-                </button> */}
             </form>
         </div>
     )
