3c174bfb0e4901084e4d5d84ad81a67d5764609b
Luis Escobar <luisaeniebla@gmail.com>
2023-08-07 16:08:31 -0700

    attempted mobile version

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,8 +3,13 @@ import EmailForm from "./EmailForm"
 
 const Card = ({ }) => {
     return (
-        <div className="w-[928px] h-[641px] bg-white rounded-[36px] p-[24px] flex justify-between items-center">
-            <div className="flex flex-col items-start max-w-[376px] max-h-[446px] mx-[40px]">
+        <div className="relative w-[375px] h-[842px] md:w-[928px] md:h-[641px] bg-white rounded-[36px]
+            flex flex-col md:flex-row md:justify-between md:mb-0 items-center"
+        >
+            <div className="md:hidden">
+                <Image src="/images/illustration-sign-up-mobile.svg" width={375} height={284} alt="" />
+            </div>
+            <div className="container md:flex md:flex-col items-start max-w-[376px] max-h-[446px] mx-[40px]">
                 <h1 className="font-bold text-[3.25rem] mb-[20px]">Stay updated!</h1>
                 <p className="text-[16px] mb-[24px]">Join 60,000+ product managers receiving monthly updates on:</p>
                 <ul className="">
@@ -14,7 +19,7 @@ const Card = ({ }) => {
                 </ul>
                 <EmailForm />
             </div>
-            <div>
+            <div className="hidden md:block">
                 <Image src="/images/illustration-sign-up-desktop.svg" width={400} height={593} alt="" />
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,22 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "24px",
+    },
     extend: {
       colors: {
         "charcoal-gray": "hsl(235, 18%, 26%)",
         "dark-grey-slate": "hsl(234, 29%, 20%)",
         "my-grey": "hsl(231, 7%, 80%)",
-        "tomato": "hsl(4, 100%, 67%)",
+        tomato: "hsl(4, 100%, 67%)",
       },
       content: {
-        "bullet": 'url("/images/icon-success.svg")',
+        bullet: 'url("/images/icon-success.svg")',
       },
       fontFamily: {
-        'roboto': ["--font-roboto"],
+        roboto: ["--font-roboto"],
       },
     },
   },
